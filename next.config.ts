4586559cde7c7159329763a9854b7e7fd7873a86
Aman Patel <ap2787336@gmail.com>
2025-05-30 17:58:48 +0530

    Initial comit

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,11 @@
-
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
 import type { NextConfig } from "next";
-import type { Configuration } from "webpack";
 
 const nextConfig: NextConfig = {
-  webpack: (config: Configuration) => {
-    config.cache = false;
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
+
+
 
 
